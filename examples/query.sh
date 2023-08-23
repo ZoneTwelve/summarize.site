ada82d56c47d4f61db572c90102e3afbe3b3f9cb
ZoneTwelve <zonetwelve159@gmail.com>
2023-08-23 18:30:59 +0800

    [update] a message2json function

--- examples/query.sh
+++ examples/query.sh
@@ -2,3 +2,43 @@
 
 URL="https://chat.openai.com/backend-api/conversation"
 
+
+#!/bin/bash
+
+uuidv4() {
+  uuidgen
+}
+
+message2Json() {
+  local question="$1"
+
+  local messageJson=$(cat <<EOF
+{
+  "action": "next",
+  "messages": [
+    {
+      "id": "$(uuidv4)",
+      "author": {
+        "role": "user"
+      },
+      "role": "user",
+      "content": {
+        "content_type": "text",
+        "parts": ["$question"]
+      }
+    }
+  ],
+  "model": "text-davinci-002-render",
+  "parent_message_id": "$(uuidv4)"
+}
+EOF
+)
+
+  echo "$messageJson"
+}
+
+
+echo "Enter your question: "
+read question
+messageJson=$(message2Json "$question")
+echo "$messageJson"
